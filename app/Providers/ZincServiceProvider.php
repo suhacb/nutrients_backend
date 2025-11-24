7ea7098c6d393e14cc32773c518a516a84cd6ea5
Blaž Suhač <blazsuhac@mini.local>
2025-11-24 22:06:43 +0100

    Added SearchServiceProvider and implemented ZincSearchService.

---
+++ app/Providers/ZincServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Search\SearchServiceContract;
+use App\Services\Search\ZincSearchService;
+use Illuminate\Support\ServiceProvider;
+
+class ZincServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->app->singleton(SearchServiceContract::class, function ($app) {
+            return new ZincSearchService([
+                'base_uri' => config('zinc.base_url'),
+                'username' => config('zinc_username'),
+                'password' => config('zinc.password')
+            ]);
+        });
+    }
+}
---
+++ app/Services/Search/ZincSearchService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services\Search;
+
+use Illuminate\Support\Facades\Http;
+
+class ZincSearchService implements SearchServiceContract
+{
+    protected string $baseUri;
+    protected string $username;
+    protected string $password;
+
+    public function __construct(array $config)
+    {
+        $this->baseUri = $config['base_uri'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+    }
+
+    protected function auth()
+    {
+        return [
+            'auth' => [$this->username, $this->password]
+        ];
+    }
+
+    public function insert(string $index, array $document): bool
+    {
+        $response = Http::withBasicAuth($this->username, $this->password)
+            ->post("{$this->baseUri}/api/{$index}/_doc", $document);
+
+        return $response->successful();
+    }
+
+    public function update(string $index, string|int $id, array $document): bool
+    {
+        $response = Http::withBasicAuth($this->username, $this->password)
+            ->put("{$this->baseUri}/api/{$index}/_doc/{$id}", $document);
+
+        return $response->successful();
+    }
+
+    public function delete(string $index, string|int $id): bool
+    {
+        $response = Http::withBasicAuth($this->username, $this->password)
+            ->delete("{$this->baseUri}/api/{$index}/_doc/{$id}");
+
+        return $response->successful();
+    }
+
+    public function search(string $index, array $query, int $limit = 10, int $offset = 0): array
+    {
+        $body = [
+            'from' => $offset,
+            'size' => $limit,
+            'query' => $query
+        ];
+
+        $response = Http::withBasicAuth($this->username, $this->password)
+            ->post("{$this->baseUri}/api/{$index}/_search", $body);
+
+        return $response->json() ?? [];
+    }
+}
