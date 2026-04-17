89d46b31ca7e17d5e84e3910b6119f34fb983673
Blaž Suhač <blaz@suhac.eu>
2026-04-17 13:00:15 +0200

    Added HasMany relationship between Nutrient and Source.

--- app/Models/Source.php
+++ app/Models/Source.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Source extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'name',
         'slug',
         'url',
-        'description'
+        'description',
     ];
+
+    public function nutrients(): HasMany
+    {
+        return $this->hasMany(Nutrient::class);
+    }
 }
